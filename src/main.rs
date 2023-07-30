696403d3f48c44477d47b040ff9f309a285048b5
IEATDIRT <ieatdirt@dirt-home.home>
2023-07-30 04:07:43 -0400

    added a thing that panics if the input is 0, as well as comments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use text_io::read; 
 fn main() {
-    let mut input: i64 = read!();
+    let mut input: i64 = read!(); //make input be what you input
+    if input == 0 { 
+        println!("not a good idea") // tell you not to input 0 
+        panic!() // self explanatory
+    }
     loop{
     let checkeven: i64 = if let 0=input%2{
-        1
+        1 //define checkeven as 1 if input is even (i have no idea how this works its just code i
+          //stole from a website
     } else {
-        0
+        0 //define checkeven as 0 if it's not even 
     };
     let output: i64 = if checkeven == 1{
-        input/2
+        input/2 //divide by 2 if checkeven is 1
     } else {
-        input*3+1
+        input*3+1 //times 3 plus one if checkeven is 0
     };
     println!("{}", output);
     input=output;
